ea0ab7d188487d4c4b0d0c6b29c8e08b86d7c895
asajbel <serdan66@gmail.com>
2017-11-02 21:39:49 -0700

    first draft

--- README.md
+++ README.md
@@ -1,1 +1,24 @@
-# liri-node-app
+# LIRI
+
+Runs on node.
+
+## Installation
+
+```
+npm install request
+npm install twitter
+npm install --save node-spotify-api
+```
+
+## Use
+
+Enter one of these four commands
+
+```
+my-tweets {twitter screen name}
+spotify-this-song {song name}
+movie-this {movie title}
+do-what-it-says
+```
+
+All of the commands arguments are optional.
--- keys.js
+++ keys.js
@@ -1,17 +1,12 @@
-console.log('keys is loaded');
-
-var twitterKeys = {
-  consumer_key: 'zYeYKgwJejFou2wDWLmmN18ae',
-  consumer_secret: '0P1l7NXUE5pFYx7YhJ8o05ZKvNG313LinlNro9VXcdTpOnsRYa',
-  access_token_key: '304919828-5mTW7Z4VjwVUp5ea2DBwQW6ZE0ddaXmLT54G1Yw2',
-  access_token_secret: 'rP3jR7Ovw3XTw8nTa4aC9Npn0C3P4bQ0TUw6Qcl4z6T5o',
-}
-
-var spotifyKeys = {
-	client_id: "<insert here>",
-	client_secret: "<insert here>"
-}
-
 module.exports = {
-	twitKeys: twitterKeys,
+  twitKeys: twitterKeys = {
+    consumer_key: 'zYeYKgwJejFou2wDWLmmN18ae',
+    consumer_secret: '0P1l7NXUE5pFYx7YhJ8o05ZKvNG313LinlNro9VXcdTpOnsRYa',
+    access_token_key: '304919828-5mTW7Z4VjwVUp5ea2DBwQW6ZE0ddaXmLT54G1Yw2',
+    access_token_secret: 'rP3jR7Ovw3XTw8nTa4aC9Npn0C3P4bQ0TUw6Qcl4z6T5o',
+  },
+  spotKeys: spotifyKeys = {
+    id: "5ec67e9b1a42475cb0cac922a45dbfaf",
+    secret: "c2548653afc343b48c4e830c18366f86"
+  }
 }
